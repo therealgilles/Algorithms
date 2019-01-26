ca1d932558a1f0e70b0c9ea2bd76977228334163
Gilles BOUVIER <gillesbouvier+github@gmail.com>
2019-01-25 18:03:52 -0800

    Put test code under if debug statement.

--- lowestCommonAncestor/lowestCommonAncestor.js
+++ lowestCommonAncestor/lowestCommonAncestor.js
@@ -66,54 +66,59 @@ const lowestCommonAncestor = (p1, p2) => {
   return null
 }
 
-/*
- *            p1        p2
- *          p3  p4    p5  p6
- *        p7 p8  p9 p10    p11
- *      p12  p13  p14    p15 p16
- *
- */
+const debug = false
 
-const p1 = new Person('p1')
-const p2 = new Person('p2')
-const p3 = new Person('p3')
-const p4 = new Person('p4')
-const p5 = new Person('p5')
-const p6 = new Person('p6')
-const p7 = new Person('p7')
-const p8 = new Person('p8')
-const p9 = new Person('p9')
-const p10 = new Person('p10')
-const p11 = new Person('p11')
-const p12 = new Person('p12')
-const p13 = new Person('p13')
-const p14 = new Person('p14')
-const p15 = new Person('p15')
-const p16 = new Person('p16')
+if (debug) {
 
-p1.parents = []
-p2.parents = []
-p3.parents = [p1]
-p4.parents = [p1]
-p5.parents = [p2]
-p6.parents = [p2]
-p7.parents = [p3]
-p8.parents = [p3]
-p9.parents = [p4]
-p10.parents = [p5]
-p11.parents = [p6]
-p12.parents = [p7]
-p13.parents = [p8]
-p14.parents = [p9, p10]
-p15.parents = [p11]
-p16.parents = [p11]
+  /*
+   *            p1        p2
+   *          p3  p4    p5  p6
+   *        p7 p8  p9 p10    p11
+   *      p12  p13  p14    p15 p16
+   *
+   */
 
-console.log('p14/p1 =', lowestCommonAncestor(p14, p1)) // p1
-console.log('p14/p2 =', lowestCommonAncestor(p14, p2)) // p2
-console.log('p14/p13 =', lowestCommonAncestor(p14, p13)) // p1
-console.log('p14/p12 =', lowestCommonAncestor(p14, p12)) // p1
-console.log('p12/p15 =', lowestCommonAncestor(p12, p15)) // undefined
-console.log('p3/p6 =', lowestCommonAncestor(p3, p6)) // undefined
-console.log('p12/p8 =', lowestCommonAncestor(p12, p8)) // p3
-console.log('p3/p13 =', lowestCommonAncestor(p3, p13)) // p3
+  const p1 = new Person('p1')
+  const p2 = new Person('p2')
+  const p3 = new Person('p3')
+  const p4 = new Person('p4')
+  const p5 = new Person('p5')
+  const p6 = new Person('p6')
+  const p7 = new Person('p7')
+  const p8 = new Person('p8')
+  const p9 = new Person('p9')
+  const p10 = new Person('p10')
+  const p11 = new Person('p11')
+  const p12 = new Person('p12')
+  const p13 = new Person('p13')
+  const p14 = new Person('p14')
+  const p15 = new Person('p15')
+  const p16 = new Person('p16')
+
+  p1.parents = []
+  p2.parents = []
+  p3.parents = [p1]
+  p4.parents = [p1]
+  p5.parents = [p2]
+  p6.parents = [p2]
+  p7.parents = [p3]
+  p8.parents = [p3]
+  p9.parents = [p4]
+  p10.parents = [p5]
+  p11.parents = [p6]
+  p12.parents = [p7]
+  p13.parents = [p8]
+  p14.parents = [p9, p10]
+  p15.parents = [p11]
+  p16.parents = [p11]
+
+  console.log('p14/p1 =', lowestCommonAncestor(p14, p1)) // p1
+  console.log('p14/p2 =', lowestCommonAncestor(p14, p2)) // p2
+  console.log('p14/p13 =', lowestCommonAncestor(p14, p13)) // p1
+  console.log('p14/p12 =', lowestCommonAncestor(p14, p12)) // p1
+  console.log('p12/p15 =', lowestCommonAncestor(p12, p15)) // undefined
+  console.log('p3/p6 =', lowestCommonAncestor(p3, p6)) // undefined
+  console.log('p12/p8 =', lowestCommonAncestor(p12, p8)) // p3
+  console.log('p3/p13 =', lowestCommonAncestor(p3, p13)) // p3
+}
 
